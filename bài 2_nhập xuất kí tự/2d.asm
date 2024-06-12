38c22b1c846063f12f2618c22fd48ae3cb45fa86
leave-me-alone <130025702+me-413@users.noreply.github.com>
2024-06-13 00:47:08 +0700

    Create 2d.asm

---
+++ bài 2_nhập xuất kí tự/2d.asm
@@ -0,0 +1,27 @@
+dseg segment
+max db 30
+len db 0
+chuoi db 30, 0, 30 dup(?)
+tbao db 'Hay nhap vao tu ban phim chuoi ky tu... $'
+dseg ends
+
+cseg segment
+assume cs:cseg, ds:dseg
+
+start:
+mov ax, dseg
+mov ds, ax
+
+mov ah, 09h
+lea dx, tbao
+int 21h
+
+mov ah, 0Ah
+lea dx, chuoi
+int 21h
+
+mov ah, 4Ch
+int 21h
+
+cseg ends
+end start
