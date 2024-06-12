376203f1b9408c060dffdc9708894f00c4450622
leave-me-alone <130025702+me-413@users.noreply.github.com>
2024-06-13 00:47:45 +0700

    Update 2c.asm

--- bài 2_nhập xuất kí tự/2c.asm
+++ bài 2_nhập xuất kí tự/2c.asm
@@ -1,28 +1,29 @@
 dseg segment
-max db 30
-len db 0
-chuoi db 30, 0, 30 dup(?)
-tbao db 'Hay nhap vao tu ban phim chuoi ky tu... $'
+tbao db 'Hay nhap vao mot ky tu bat ky ... $' ; dau $ de thong bao cho assembler het chuoi
 dseg ends
 
+; bien tbao duoc khai bao voi cu phap <ten_bien> <kieu_du_lieu> <gia_tri>
+; kieu_du_lieu co 2 kieu db la byte ; dw la word; word to hon byte thoi chu ko co j
+
 cseg segment
-assume cs:cseg, ds:dseg
+assume cs:cseg, ds:dseg ; cseg la doan lenh, dseg la doan du lieu
+cseg ends
+
+; dong assume .. -> thong bao cho assembler thanh ghi cs tro den cseg, ds tro den dseg -> thiet lap ngu canh
 
 start:
-mov ax, dseg
+mov ax, dseg  ; khoi dong dia chi doan du lieu
 mov ds, ax
 
-mov ah, 09h
-lea dx, tbao
-int 21h
+mov ah, 09h   ; ham 9, in ra mot chuoi ky tu
+lea dx, tbao  ; dx <- dia chi offset cua bien tbao   (?) bat buoc phai la dx khong
+int 21h        ; goi ngat de thuc hien ham
 
-mov ah, 0Ah
-lea dx, chuoi
-int 21h
+mov ah, 01h  ; ham 1, cho user an phim bat ky
+int 21h  
 
-mov ah, 4Ch
+mov ah, 4Ch  ; tro ve he dieu hanh
 int 21h
 
 cseg ends
 end start
-
